4d48c6cd446da29051c9d84e867a316bd809bb09
Jonathan Müller <jonathanmueller.dev@gmail.com>
2016-04-19 16:36:35 +0200

    Basic CMake setup

---
+++ tool/main.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2016 Jonathan Müller <jonathanmueller.dev@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
+int main(int argc, char *argv[])
+{
+
+}
