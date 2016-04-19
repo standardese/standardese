c1359c56ebb4b81e0d3fee7e69c1c8e89bebfc2d
Jonathan Müller <jonathanmueller.dev@gmail.com>
2016-04-19 19:26:12 +0200

    Add support for configuration options that can be read from file as well

--- tool/main.cpp
+++ tool/main.cpp
@@ -3,6 +3,7 @@
 // found in the top-level directory of this distribution.
 
 #include <cassert>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -17,19 +18,24 @@ void print_version(const char *exe_name)
     std::clog << "Copyright (C) 2016 Jonathan Müller <jonathanmueller.dev@gmail.com>\n";
 }
 
-void print_usage(const char *exe_name, const po::options_description &generic)
+void print_usage(const char *exe_name,
+                 const po::options_description &generic,
+                 const po::options_description &configuration)
 {
     std::clog << "Usage: " << exe_name << " [options] inputs\n";
     std::clog << '\n';
     std::clog << generic << '\n';
+    std::clog << '\n';
+    std::clog << configuration << '\n';
 }
 
 int main(int argc, char** argv)
 {
-    po::options_description generic("Generic options");
+    po::options_description generic("Generic options"), configuration("Configuration");
     generic.add_options()
             ("version,v", "prints version information and exits")
-            ("help,h", "prints this help message and exits");
+            ("help,h", "prints this help message and exits")
+            ("config,c", po::value<std::string>(), "read options from additional config file as well");
 
     po::options_description input("");
     input.add_options()
@@ -38,30 +44,44 @@ int main(int argc, char** argv)
     input_pos.add("input-files", -1);
 
     po::options_description cmd;
-    cmd.add(generic).add(input);
+    cmd.add(generic).add(configuration).add(input);
 
     po::variables_map map;
     try
     {
         po::store(po::command_line_parser(argc, argv).options(cmd).positional(input_pos).run(), map);
         po::notify(map);
+
+        auto iter = map.find("config");
+        if (iter != map.end())
+        {
+            auto path = iter->second.as<std::string>();
+            std::ifstream config(path);
+            if (!config.is_open())
+                throw std::runtime_error("config file '" + path + "' not found");
+
+            po::options_description conf;
+            conf.add(configuration);
+            po::store(po::parse_config_file(config, configuration), map);
+            po::notify(map);
+        }
     }
-    catch (boost::program_options::error &ex)
+    catch (std::exception &ex)
     {
         std::cerr << "Error: " << ex.what() << '\n';
-        print_usage(argv[0], generic);
+        print_usage(argv[0], generic, configuration);
         return 1;
     }
 
     if (map.count("help"))
-        print_usage(argv[0], generic);
+        print_usage(argv[0], generic, configuration);
     else if (map.count("version"))
         print_version(argv[0]);
     else if (map.count("input-files") == 0u)
     {
         std::cerr << "Error: no input file specified\n";
         std::cerr << '\n';
-        print_usage(argv[0], generic);
+        print_usage(argv[0], generic, configuration);
         return 1;
     }
     else
