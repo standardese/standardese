39e462a4425317a10377b5fc098e24327d39d9a5
Julian Rüth <julian.rueth@fsfe.org>
2021-02-25 18:16:13 +0100

    fix catch include patch

--- test/util/assertions/sections.cpp
+++ test/util/assertions/sections.cpp
@@ -2,7 +2,7 @@
 // This file is subject to the license terms in the LICENSE file
 // found in the top-level directory of this distribution.
 
-#include <catch.hpp>
+#include "../../external/catch/single_include/catch2/catch.hpp"
 
 #include "sections.hpp"
 #include "../indent.hpp"
